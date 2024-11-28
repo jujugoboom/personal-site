193c92dff347ae23a48547a1a3b4216d181564f5
jujugoboom <jujugoboom@gmail.com>
2024-11-28 13:45:47 -0800

    Add dark mode toggle

--- src/store.ts
+++ src/store.ts
@@ -4,3 +4,10 @@ export const isSimpleFonts = persistentAtom<boolean>('isSimpleFonts', false, {
     encode: JSON.stringify,
     decode: JSON.parse,
 })
+
+export const darkMode = persistentAtom<boolean>(
+    'darkMode',
+    window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches,
+    { encode: JSON.stringify, decode: JSON.parse }
+)
