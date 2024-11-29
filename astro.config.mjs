d8705bdcbfc5775edac0bda179e75fc0f774513e
jujugoboom <jujugoboom@gmail.com>
2024-11-29 10:35:16 -0800

    Refactor styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
     site: 'https://justincovell.com',
-    integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+    integrations: [tailwind(), sitemap()],
     output: 'static',
 })
