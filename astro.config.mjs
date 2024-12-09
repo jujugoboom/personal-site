74e97b55ba71df0fd08f124a60d8d4935f090c2f
jujugoboom <jujugoboom@gmail.com>
2024-12-09 00:01:57 -0800

    add css inlining

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from '@astrojs/tailwind'
 
 import sitemap from '@astrojs/sitemap'
 
+import playformInline from '@playform/inline'
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://justincovell.com',
-    integrations: [tailwind(), sitemap()],
+    integrations: [tailwind(), sitemap(), playformInline({})],
     output: 'static',
     markdown: {
         shikiConfig: {
